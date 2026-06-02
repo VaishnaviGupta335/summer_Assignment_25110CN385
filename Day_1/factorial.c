8471fddc24c574d44f984910dce722d011d81d42
VaishnaviGupta335 <vaishug335@gmail.com>
2026-06-02 10:23:05 +0530

    Day-1 assignment is uploaded

--- Day_1/factorial.c
+++ Day_1/factorial.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int n,i=2,fact=1;
+    int n, i = 1, fact = 1;
     printf("Enter the number: ");
     scanf("%d", &n);
-    if (n==0 || n==1)
+    while (i <= n)
     {
-        printf("Factorial of %d is 1", n);
+        fact = fact * i;
+        i = i + 1;
     }
-    else
-    {
-        while(i<=n)
-        {
-            fact=fact*i;
-            i=i+1;
-        }
-        printf("Factorial of %d is %d", n,fact);
-    }
+    printf("Factorial of %d is %d", n, fact);
     return 0;
 }
