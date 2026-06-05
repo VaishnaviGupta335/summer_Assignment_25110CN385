36853e9e6196e15436682f0b95804da1f333fa56
VaishnaviGupta335 <vaishug335@gmail.com>
2026-06-05 15:05:39 +0530

    Day-8 assignment is uploaded

--- Day_8/number_half_pyramid2.c
+++ Day_8/number_half_pyramid2.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int n;
     printf("enter the number of rows: ");
     scanf("%d", &n);
-    for(int i=1; i<=5; i++){
-        for(int j=1; j<=i;j++){
-            printf("%d",i);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", i);
         }
         printf("\n");
     }
