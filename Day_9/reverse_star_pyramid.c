37f859c29da859e898b3c3d162fcf77b4e493aa6
VaishnaviGupta335 <vaishug335@gmail.com>
2026-06-05 15:26:18 +0530

    Day-9 assignment is uploaded

---
+++ Day_9/reverse_star_pyramid.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("enter the number of rows: ");
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        for(int j=n+1-i; j>=1;j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
